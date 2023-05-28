01de4fc80e367b5c25f06648522699324047c0b2
vujovicm <63158265+vujovicm@users.noreply.github.com>
2023-05-29 00:15:34 +0200

    Update welcomePage.js

--- client/src/pages/welcomePage.js
+++ client/src/pages/welcomePage.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 const Welcome = () => {
+
+    let navigate = useNavigate();
+    const goToMessagePage = () => {
+        let path= "/message";
+        navigate(path);
+    }; 
+    
     return (
         <>
         <h1> Guestbook </h1>
         <h3> See what people wrote about us and leave a message.</h3>
-        <button> Leave a message </button></>
+
+        <button onClick= { goToMessagePage }> Leave a message </button></>
     );
 };
 
